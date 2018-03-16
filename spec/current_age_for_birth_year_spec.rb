0e80b8fdd6211a9f6fd54767a024db11d7102e3a
curiositypaths <curiositypaths@users.noreply.github.com>
2018-03-16 17:46:30 -0400

    Correct solution

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -2,8 +2,10 @@ require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
-    age_of_person = current_age_for_birth_year(1984)
+    now = Time.new("2018-01-01 13:00:00")
+    allow(Time).to receive(:now) { now }
+    age_of_person = current_age_for_birth_year(1998)
 
-    expect(age_of_person).to eq(19)
+    expect(age_of_person).to eq(20)
   end
 end
