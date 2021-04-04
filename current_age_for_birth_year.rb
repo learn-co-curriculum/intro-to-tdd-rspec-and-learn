f84759a9b538e4831c31beccd75d06087140fafc
Drew Keat <thedrewkeat@gmail.com>
2021-04-04 15:54:50 -0600

    Completed lesson

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,3 @@
+def current_age_for_birth_year(year)
+    return Time.now.year - year
+end
---
+++ how_old_are_you.rb
@@ -0,0 +1,8 @@
+require_relative './current_age_for_birth_year.rb'
+
+puts "What year were you born?"
+birth_year = gets.to_i
+
+users_age = current_age_for_birth_year(birth_year)
+
+puts "You are: #{users_age} years old."
